462a26d1e058f40c6a8ea60d9e9fcd8782e78201
makanika <degwell@gmail.com>
2024-04-16 18:20:59 +0300

    Addedd Express JS to the application

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const http = require('http')
-const routes = require('./routes')
+const express = require("express");
 
-const server = http.createServer(routes)
+// Run express as a function
+const app = express();
 
-server.listen(3000)
+// Adding a middleware function in express
+app.use((req, res, next) => {
+  console.log("First Express Expression");
+  next(); // Goes to the next middleware
+});
+
+app.use((req, res, next) => {
+  console.log("The Next Middleware from my express application");
+  res.send("<h1>Greetings from my first express lesson</h1>");
+});
+
+app.listen(3000);
